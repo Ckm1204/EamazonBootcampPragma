e29657c8a7874d7ad5a0e5f93fa4c6feb9fd72e6
Camilo Tello <camilotello04@gmail.com>
2024-08-16 12:23:54 -0500

    changes  bd

--- Stock/Stock BD.sql
+++ Stock/Stock BD.sql
@@ -1,30 +1,39 @@
 CREATE DATABASE stock;
 USE stock;
+
 CREATE TABLE Category (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE,
-    description VARCHAR(90) NOT NULL
+                          id INT AUTO_INCREMENT PRIMARY KEY,
+                          name VARCHAR(50) NOT NULL UNIQUE,
+                          description VARCHAR(90) NOT NULL,
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Brand (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE,
-    description VARCHAR(120) NOT NULL
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       name VARCHAR(50) NOT NULL UNIQUE,
+                       description VARCHAR(120) NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Article (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(255) NOT NULL,
+                         description TEXT NOT NULL,
+                         quantity INT NOT NULL,
+                         price DECIMAL(10, 2) NOT NULL,
+                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Article_Category (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    article_id INT NOT NULL,
-    category_id INT NOT NULL,
-    FOREIGN KEY (article_id) REFERENCES Article(id),
-    FOREIGN KEY (category_id) REFERENCES Category(id),
-    UNIQUE (article_id, category_id) -- Evita categorías duplicadas por artículo
+                                  id INT AUTO_INCREMENT PRIMARY KEY,
+                                  article_id INT NOT NULL,
+                                  category_id INT NOT NULL,
+                                  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                  FOREIGN KEY (article_id) REFERENCES Article(id),
+                                  FOREIGN KEY (category_id) REFERENCES Category(id),
+                                  UNIQUE (article_id, category_id) -- Evita categorías duplicadas por artículo
 );
