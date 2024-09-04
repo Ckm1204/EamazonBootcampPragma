52c010e4618e82c97f2742a7aae280cf1db6ed00
Camilo Tello <camilotello04@gmail.com>
2024-09-03 23:31:57 -0500

    cambios jpa adapter

--- Stock/src/main/java/com/Eamazon/Stock/infraestructure/out/jpa/adapter/BrandJpaAdapter.java
+++ Stock/src/main/java/com/Eamazon/Stock/infraestructure/out/jpa/adapter/BrandJpaAdapter.java
@@ -38,9 +38,6 @@ public class BrandJpaAdapter implements IBrandPersistencePort {
         return brandMapper.toBrandList(brandEntityList);
     }
 
-
-
-
     @Override
     public Optional<BrandModelRequest> findByName(String name) {
         return brandRepository.findByName(name)
