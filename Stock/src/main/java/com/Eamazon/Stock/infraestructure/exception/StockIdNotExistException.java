0ff59f96a4837d38804a091546d4f351bfabdf16
Camilo Tello <camilotello04@gmail.com>
2024-10-05 11:35:43 -0500

    add stock completado

--- Stock/src/main/java/com/Eamazon/Stock/infraestructure/exception/StockIdNotExistException.java
+++ Stock/src/main/java/com/Eamazon/Stock/infraestructure/exception/StockIdNotExistException.java
@@ -4,4 +4,6 @@ public class StockIdNotExistException extends RuntimeException{
     public StockIdNotExistException() {
         super();
     }
+
+
 }
--- Stock/src/main/java/com/Eamazon/Stock/infraestructure/input/rest/AddStockRestController.java
+++ Stock/src/main/java/com/Eamazon/Stock/infraestructure/input/rest/AddStockRestController.java
@@ -3,6 +3,11 @@ package com.Eamazon.Stock.infraestructure.input.rest;
 import com.Eamazon.Stock.application.dto.request.AddStock;
 import com.Eamazon.Stock.application.service.Item.ItemService;
 import com.Eamazon.Stock.infraestructure.input.rest.role.RolePermissionConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -21,11 +26,19 @@ public class AddStockRestController {
         this.itemService = itemService;
     }
 
-    // Add stock to an item
+    @Operation(summary = "Add stock to an item")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Stock added successfully", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Invalid request parameters", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @PreAuthorize(RolePermissionConstants.HAS_ROLE_AUX_BODEGA)
     @PostMapping("")
-    public ResponseEntity<Void> addStockToItem(@RequestBody AddStock addStock) {
-        itemService.addStock( addStock);
+    public ResponseEntity<Void> addStockToItem(
+            @Parameter(description = "Details of the stock to be added", required = true)
+            @RequestBody AddStock addStock) {
+        itemService.addStock(addStock);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
 }
